7eb2e5bbe46a7b672baab01251147f25e6aa69d1
Anthony DeMatteo <dematta@sunypoly.edu>
2025-02-19 15:21:57 -0500

    - Exercsises03.cpp: Framebuffers, CommandPool, etc.

--- src/app/Exercises03.cpp
+++ src/app/Exercises03.cpp
@@ -6,7 +6,21 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <VKUtility.hpp>
+#include "VKBuffer.hpp"
 using namespace std;
+
+struct PointVertex {
+    glm::vec3 pos;
+};
+
+vector<PointVertex> vertices = {
+    {{-0.5f, -0.5f, 0.5f}},
+    {{ 0.5f, -0.5f, 0.5f}},
+    {{ 0.5f,  0.5f, 0.5f}},
+    {{-0.5f,  0.5f, 0.5f}}
+};
+
+vector<unsigned int> indices = {0, 2, 1, 2, 0, 3};
 
 int main(int argc, char **argv) {
     if(argc >= 2) {
@@ -40,13 +54,13 @@ int main(int argc, char **argv) {
     vkb::PhysicalDevice vkbPhyDevice = phyRet.value();
     vk::PhysicalDevice phyDevice
         = vk::PhysicalDevice {vkbPhyDevice.physical_device};
+    
+    cout << "DEVICE: " << phyDevice.getProperties().deviceName << endl;    
 
     vkb::DeviceBuilder devBuilder { vkbPhyDevice };
     auto devRet = devBuilder.build();
     vkb::Device vkbDevice = devRet.value();
     vk::Device device = vk::Device {vkbDevice.device };
-
-    cout << "DEVICE: " << phyDevice.getProperties().deviceName << endl;
 
     auto graphQRet = vkbDevice.get_queue(vkb::QueueType::graphics);
     auto presQRet = vkbDevice.get_queue(vkb::QueueType::present);
@@ -120,14 +134,281 @@ int main(int argc, char **argv) {
         "build/compiledshaders/Exercises03/shader.frag.spv"
     );
 
+    vk::ShaderModule vertShader = createVulkanShaderModule(device, vertSrc);
+    vk::ShaderModule fragShader = createVulkanShaderModule(device, fragSrc);
+
+    vk::PipelineShaderStageCreateInfo shaderStages[] = {
+        vk::PipelineShaderStageCreateInfo(
+            {}, vk::ShaderStageFlagBits::eFragment, fragShader, "main" 
+        )
+    };
+
+    vk::DeviceSize vertBufferSize = sizeof(vertices[0])*vertices.size();
+    vk::DeviceSize indBufferSize = sizeof(indices[0])*indices.size();
+
+    VulkanBuffer vkVertices
+        = createVulkanBuffer(
+            phyDevice, device,
+            vertBufferSize,
+            vk::BufferUsageFlagBits::eVertexBuffer,
+            vk::MemoryPropertyFlagBits::eHostVisible
+            | vk::MemoryPropertyFlagBits::eHostCoherent);
+    VulkanBuffer vkIndices
+        = createVulkanBuffer(
+            phyDevice, device,
+            indBufferSize, 
+            vk::BufferUsageFlagBits::eIndexBuffer,
+            vk::MemoryPropertyFlagBits::eHostVisible
+            | vk::MemoryPropertyFlagBits::eHostCoherent);
+
+    copyDataToVulkanBuffer( device, 
+                    vkVertices.memory, 
+                    vertBufferSize,
+                    vertices.data());
+    copyDataToVulkanBuffer( device, 
+                    vkIndices.memory, 
+                    indBufferSize,
+                    indices.data());
+
+    vk::VertexInputBindingDescription vertBindDesc 
+    = vk::VertexInputBindingDescription(
+    0, sizeof(vertices[0]), vk::VertexInputRate::eVertex);
+
+    vector<vk::VertexInputAttributeDescription> vertAttribDesc 
+    = {
+    vk::VertexInputAttributeDescription(
+        0, 0, 
+        vk::Format::eR32G32B32Sfloat,
+        offsetof(PointVertex, pos))
+    };
+
+    vk::PipelineVertexInputStateCreateInfo vertexInputInfo(
+    {}, vertBindDesc, vertAttribDesc
+    );
+
+    vk::PipelineInputAssemblyStateCreateInfo inputAssembly(
+    {}, vk::PrimitiveTopology::eTriangleList, false
+    );
+
+    vk::Viewport viewport(
+    0, 0, (float)swapextent.width, (float)swapextent.height,
+    0.0f, 1.0f
+    );
+    vk::Rect2D scissors({0,0}, swapextent);
+    vk::PipelineViewportStateCreateInfo viewportInfo(
+    {}, viewport, scissors
+    );
+
+    vector<vk::DynamicState> dynamicStates = {
+    vk::DynamicState::eViewport,
+    vk::DynamicState::eScissor
+    };
+    vk::PipelineDynamicStateCreateInfo dynamicInfo(
+    {}, dynamicStates
+    );
+
+    vk::PipelineRasterizationStateCreateInfo
+    rasterizer {};
+    rasterizer.lineWidth = 1.0f;
+    rasterizer.cullMode
+    = vk::CullModeFlagBits::eNone;
+    rasterizer.frontFace
+    = vk::FrontFace::eCounterClockwise;
+
+    vk::PipelineColorBlendAttachmentState colorBlend {};
+    colorBlend.colorWriteMask = 
+    vk::ColorComponentFlagBits::eR |
+    vk::ColorComponentFlagBits::eG |
+    vk::ColorComponentFlagBits::eB |
+    vk::ColorComponentFlagBits::eA;
+    vk::PipelineColorBlendStateCreateInfo colorBlending(
+    {}, false, vk::LogicOp::eCopy, colorBlend
+    );
+
+    vk::PipelineDepthStencilStateCreateInfo depthStencil (
+    {}, true, true, vk::CompareOp::eLess,
+    false, false, {}, {}
+    );
+
+    vk::PipelineLayoutCreateInfo layoutInfo(
+    {}, {}, {}
+    );
+    vk::PipelineLayout pipelineLayout
+    = device.createPipelineLayout(layoutInfo);
+
+    vk::PipelineMultisampleStateCreateInfo multisample(
+    {}, vk::SampleCountFlagBits::e1
+    );
+
+    vk::PipelineCache pipelineCache
+    = device.createPipelineCache({});
+
+    vk::GraphicsPipelineCreateInfo pipelineInfo(
+    vk::PipelineCreateFlags(),
+    shaderStages,
+    &vertexInputInfo,
+    &inputAssembly,
+    0,
+    &viewportInfo,
+    &rasterizer,
+    &multisample,
+    &depthStencil,
+    &colorBlending,
+    &dynamicInfo,
+    pipelineLayout,
+    pass
+    );
+    auto pipeRet = device.createGraphicsPipeline(
+    pipelineCache, pipelineInfo
+    );
+    vk::Pipeline graphicsPipeline = pipeRet.value;
+
+    device.destroyShaderModule(vertShader);
+    device.destroyShaderModule(fragShader);
+
+    vector<vk::Framebuffer> framebuffers;
+    framebuffers.resize(swapviews.size());
+    for(int i = 0; framebuffers.size(); i++) {
+        vector<vk::ImageView> attach = {
+            swapviews.at(i)
+        };
+        framebuffers[i] = device.createFramebuffer(
+            vk::FramebufferCreateInfo(
+                {},
+                pass,
+                attach,
+                swapextent.width,
+                swapextent.height
+
+            )
+        );
+    }
+
+    vk::CommandPool commandPool
+        = device.createCommandPool(
+            vk::CommandPoolCreateInfo(
+                vk::CommandPoolCreateFlags(
+                    vk::CommandPoolCreateFlagBits::eResetCommandBuffer
+                ),
+                graphIndex
+            )
+        );
+
+    vk::CommandBuffer commandBuffer
+        = device.allocateCommandBuffers(
+            vk::CommandBufferAllocateInfo(
+                commandPool, 
+                vk::CommandBufferLevel::ePrimary, 
+                1
+            )
+        ).front();
+        
+        vk::Fence inFlightFence
+            = device.createFence(
+                vk::FenceCreateInfo(
+                    vk::FenceCreateFlagBits::eSignaled)
+            );
+
+        vk::Semaphore imageSem
+        = device.createSemaphore(vk::SemaphoreCreateInfo());
+        vk::Semaphore renderSem
+        = device.createSemaphore(vk::SemaphoreCreateInfo());
+
     // CREATION TODO
 
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
+
+        device.waitForFences(1, &inFlightFence,
+                            true, UINT64_MAX);
+        device.resetFences(1, &inFlightFence);
+
+        auto result = device.acquireNextImageKHR(
+            swapchain, UINT64_MAX,
+            imageSem, nullptr
+        );
+
+        unsigned int frameIndex = result.value;
+
+        commandBuffer.reset();
+        commandBuffer.begin(vk::CommandBufferBeginInfo());
+        array<vk::ClearValue, 1> clearValues{};
+        clearValues[0].color 
+        = vk::ClearColorValue(0.9f, 0.9f, 0.0f, 1.0f);
+
+        commandBuffer.beginRenderPass(
+            vk::RenderPassBeginInfo(
+                pass, framebuffers[frameIndex],
+                { {0,0}, swapextent},
+                clearValues
+            ),
+            vk::SubpassContents::eInline
+        );
+        ///////////////////////////////
+        vk::Viewport viewports[] = 
+        {
+                {0, 0,
+                (float)swapextent.width,
+                (float)swapextent.height,
+                0.0f, 1.0f}
+        };
+        commandBuffer.setViewport(0, viewports);
+
+        vk::Rect2D scissors[] =
+        {
+            {{0,0}, swapextent}
+        };
+        commandBuffer.setScissor(0, scissors);
+
         // RENDER TODO
+
+        commandBuffer.endRenderPass();
+        commandBuffer.end();
+
+        vk::Semaphore waitList[] = {imageSem};
+        vk::Semaphore sigList[] = {renderSem};
+
+        vk::PipelineStageFlags waitStages[]
+        = {vk::PipelineStageFlagBits::eColorAttachmentOutput};
+
+        vk::SubmitInfo submitInfo(
+            waitList, 
+            waitStages,
+            commandBuffer,
+            sigList
+            
+        );
+
+        graphQueue.submit(submitInfo, inFlightFence);
+
+        vk::SwapchainKHR swapchains[] = {swapchain};
+        uint32_t imageIndices[] = {frameIndex};
+        vk::PresentInfoKHR presentInfo(
+            sigList,
+            swapchains,
+            imageIndices
+        );
+
+        presentQueue.presentKHR(presentInfo);
     }
     
     // CLEANUP TODO
+
+    device.destroySemaphore(imageSem);
+    device.destroySemaphore(renderSem);
+    device.destroyCommandPool(commandPool);
+
+    for(auto framebuffer: framebuffers) {
+        device.destroyFramebuffer(framebuffer);
+    }
+    framebuffers.clear();
+
+    device.destroyPipelineCache(pipelineCache);
+    device.destroyPipelineLayout(pipelineLayout);
+    device.destroyPipeline(graphicsPipeline);
+    cleanupVulkanBuffer(device, vkIndices);
+    cleanupVulkanBuffer(device, vkVertices);
+
     device.destroyRenderPass(pass);
     for(int i = 0; i < swapviews.size(); i++) {
         device.destroyImageView(swapviews.at(i));
